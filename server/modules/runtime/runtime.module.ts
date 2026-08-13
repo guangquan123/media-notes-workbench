b4141313213f0aa994ea445f871db27cc096842c
xiongjie <DataBlau@1>
2026-08-13 17:19:33 +0800

    feat: external AI model is primary in local mode with clear error

--- server/modules/runtime/runtime.module.ts
+++ server/modules/runtime/runtime.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { RuntimeController } from './runtime.controller';
 import { RuntimeRegistryService } from './runtime.registry.service';
 
+@Global()
 @Module({
   controllers: [RuntimeController],
   exports: [RuntimeRegistryService],
